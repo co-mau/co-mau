3cd6b1ce4cc08bb07c1fc01722c2c1e71c8bff05
co-mau <dat.co.mau@gmail.com>
2020-05-02 09:34:58 +0700

    Updates

---
+++ precache-manifest.9a2ee5412e34f5900aab88e5976b1c95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1e3ef530b77a860b2ec545775d7bc4",
+    "url": "/co-mau/index.html"
+  },
+  {
+    "revision": "dbb7b7607692a9af45a5",
+    "url": "/co-mau/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e51a0162ad94e7522a91",
+    "url": "/co-mau/static/js/2.fd9c7631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/co-mau/static/js/2.fd9c7631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb7b7607692a9af45a5",
+    "url": "/co-mau/static/js/main.bcf57509.chunk.js"
+  },
+  {
+    "revision": "b6653bd2f4956aa52961",
+    "url": "/co-mau/static/js/runtime-main.d2dd5f91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/co-mau/static/media/logo.5d5d9eef.svg"
+  }
+]);
